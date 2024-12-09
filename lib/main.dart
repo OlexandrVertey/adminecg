9ee947eae96e83e1e342bf3c83d47897d06fc227
OlexandrVertey <olexandrvertey@gmail.com>
2024-12-09 10:46:10 +0200

    connect project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:adminecg/admin_ecg_app.dart';
 import 'package:adminecg/common/di/app_dependency_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb) {
+    await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCsAsgxale4wngdWdpmj3ERo1zLjiEQIek",
+      authDomain: "ecg-practice-app.firebaseapp.com",
+      projectId: "ecg-practice-app",
+      storageBucket: "ecg-practice-app.firebasestorage.app",
+      messagingSenderId: "302817283166",
+      appId: "1:302817283166:web:eeda1317a98b486108f8cf",
+      measurementId: "G-END170J84B",
+    ),
+  );
+  }
   runAppWithInjectedDependencies(app: const MyApp());
 }
