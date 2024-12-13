38c93f1ae5b026d83fa43005ef96c374dda29e9c
VR88 <vrs.gss@gmail.com>
2024-12-13 20:25:22 +0200

    add content and topic page

--- lib/ui/main_management_page/main_management_page.dart
+++ lib/ui/main_management_page/main_management_page.dart
@@ -1,4 +1,6 @@
 import 'package:adminecg/common/theme/app_theme.dart';
+import 'package:adminecg/ui/content_management/content_management_module.dart';
+import 'package:adminecg/ui/diagnosis_topics/diagnosis_topics_module.dart';
 import 'package:adminecg/ui/user_management_page/user_management_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,6 +13,19 @@ class MainManagementPage extends StatefulWidget {
 }
 
 class _MainManagementPageState extends State<MainManagementPage> {
+  late final List<Widget> list;
+  int index = 0;
+
+  @override
+  void initState() {
+    list = [
+      const UserManagementPage(),
+      const ContentManagementModule(),
+      const DiagnosisTopicsModule(),
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,39 +34,16 @@ class _MainManagementPageState extends State<MainManagementPage> {
         padding: const EdgeInsets.all(10),
         child: Row(
           children: [
-            Container(
-              padding: const EdgeInsets.all(25),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(26.0)),
-                border: Border.all(color: const Color(0xffD9D9D9), width: 1.3),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Image.asset("assets/images/png/app_logo.png"),
-                  const SizedBox(height: 40),
-                  Container(
-                    width: 260,
-                    padding: const EdgeInsets.all(12),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      color: Color(0xff0C4F75),
-                    ),
-                    child: _itemManagementWidget(),
-                  ),
-                ],
-              ),
+            LeftAdminBar(
+              currentIndex: index,
+              onChange: (index) {
+                setState(() {
+                  this.index = index;
+                });
+              },
             ),
             const SizedBox(width: 100),
-            UserManagementPage(),
+            Expanded(child: list[index]),
           ],
         ),
       ),
@@ -66,11 +58,99 @@ class _MainManagementPageState extends State<MainManagementPage> {
         Text(
           'User Management',
           style: Theme.of(context).textTheme.labelSmall?.copyWith(
-            // color: AppTheme.textColorLight,
-            color: Colors.white,
-          ),
+                // color: AppTheme.textColorLight,
+                color: Colors.white,
+              ),
         ),
       ],
+    );
+  }
+}
+
+class LeftAdminBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onChange;
+
+  const LeftAdminBar({
+    super.key,
+    required this.currentIndex,
+    required this.onChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(26.0)),
+        border: Border.all(color: const Color(0xffD9D9D9), width: 1.3),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset("assets/images/png/app_logo.png"),
+          const SizedBox(height: 40),
+          _itemWidget(
+            context,
+            0,
+            'User Management',
+            'assets/images/svg/user.svg',
+          ),
+          _itemWidget(
+            context,
+            1,
+            'Content Management',
+            'assets/images/svg/user.svg',
+          ),
+          _itemWidget(
+            context,
+            2,
+            'Diagnisis & Topics',
+            'assets/images/svg/user.svg',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _itemWidget(
+    BuildContext context,
+    int index,
+    String text,
+    String assets,
+  ) {
+    bool isSelected = index == currentIndex;
+    return InkWell(
+      onTap: () => onChange(index),
+      child: Container(
+        width: 260,
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+          color: isSelected ? AppTheme.accessColor : Colors.white,
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(assets),
+            const SizedBox(width: 12),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                    // color: AppTheme.textColorLight,
+                    color: isSelected ? Colors.white : Colors.black54,
+                  ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
