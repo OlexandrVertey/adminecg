0104dc4a86a5f54a7ce88a0c334b97c215d4362f
VR88 <vrs.gss@gmail.com>
2025-01-27 12:10:42 +0200

    fix admin send

--- lib/ui/widgets/email_send_utils.dart
+++ lib/ui/widgets/email_send_utils.dart
@@ -2,34 +2,34 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> sendEmail(Map<String, dynamic> map) async {
-  // final url = Uri.parse('https://api.sendgrid.com/v3/mail/send');
-  // final response = await http.post(
-  //   url,
-  //   headers: {
-  //     'Authorization': 'Bearer QShi0DRjSNK9JDVraCBE0w',
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: jsonEncode({
-  //     'personalizations': [
-  //       {
-  //         'to': [
-  //           {'email': toEmail}
-  //         ],
-  //         'subject': subject,
-  //       }
-  //     ],
-  //     'content': [
-  //       {
-  //         'type': 'text/plain',
-  //         'value': message,
-  //       }
-  //     ],
-  //   }),
-  // );
-  //
-  // if (response.statusCode == 202) {
-  //   print('Email sent successfully!');
-  // } else {
-  //   print('Failed to send email: ${response.body}');
-  // }
+  final url = Uri.parse('https://api.sendgrid.com/v3/mail/send');
+  final response = await http.post(
+    url,
+    headers: {
+      'Authorization': 'Bearer QShi0DRjSNK9JDVraCBE0w',
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      'personalizations': [
+        {
+          'to': [
+            {'email': map['email']}
+          ],
+          'subject': 'subject',
+        }
+      ],
+      'content': [
+        {
+          'type': 'text/plain',
+          'value': 'message',
+        }
+      ],
+    }),
+  );
+
+  if (response.statusCode == 202) {
+    print('Email sent successfully!');
+  } else {
+    print('Failed to send email: ${response.body}');
+  }
 }
